c759222d6813fb1dc711e1075363d19c5c46d1f8
thoufik1111 <mohammedthoufik22@gmail.com>
2025-12-17 16:04:34 +0530

    Fix GitHub Pages white screen: Add basename, SPA routing, and 404.html

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Auticare.com">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<Navigate to="/" replace />} />
